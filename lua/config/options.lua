ac94a087937094917aad56c35a76654504f2e91c
PM <phlipmul@gmail.com>
2024-03-08 03:58:10 -0500

    trying to integrate into nix

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+--
+--  Suppress LSP zero message until it breaks too much
+vim.g.lsp_zero_extend_lspconfig = 0
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,3 +1,45 @@
+-- local lsp_zero_config = {
+--   call_servers = 'global',
+-- }
+--
+-- local lsp_servers = {
+--   'lua_ls',
+--   'volar',
+--   'nil_ls',
+--   'tailwindcss',
+-- }
+--
+-- -- local lua_ls_config = {
+-- --     settings = {
+-- --         Lua = {
+-- --             diagnostics = {globals = {'vim'}},
+-- --             runtime = {version = 'LuaJIT'},
+-- --             telemetry = {enable = false},
+-- --         },
+-- --     },
+-- -- }
+-- return {
+--   {
+--     "VonHeikemen/lsp-zero.nvim",
+--     init = function()
+--       local lsp_zero = require('lsp-zero')
+--       lsp_zero.extend_lspconfig()
+--       lsp_zero.on_attach(function(client, bufnr)
+--         -- see :help lsp-zero-keybindings
+--         -- to learn the available actions
+--         lsp_zero.default_keymaps({ buffer = bufnr })
+--       end)
+--       -- lsp_zero.set_preferences(lsp_zero_config)
+--
+--       -- lsp_zero.configure('lua_ls', lua_ls_config)
+--
+--       lsp_zero.setup_servers(lsp_servers)
+--       lsp_zero.setup()
+--     end,
+--     dependencies = { { "neovim/nvim-lspconfig" } }
+--   },
+-- }
+
 return {
   {
     "neovim/nvim-lspconfig",
