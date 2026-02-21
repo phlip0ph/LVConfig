6335bf05a30603d3994be0286e012b854dbf04da
Phlip0ph <phlipmul@gmail.com>
2026-02-21 01:46:52 -0500

    added new custom open function

--- lua/functions/openPhrends.lua
+++ lua/functions/openPhrends.lua
@@ -1,4 +1,14 @@
--- TODO: Get the current file path
--- TODO: Break it down to the right path (everything after phrends)
--- TODO: Cutoff file extensions
--- TODO: Prepend localhost:4321
+local function open_phrends_document()
+  local fullPath = vim.fn.expand("%")
+  local cutPath = string.match(fullPath, "/phrends.+")
+  local noExt = string.gsub(cutPath, "%..+", "")
+  local link = " http://localhost:4321" .. noExt
+  print("Opening: " .. link)
+  vim.cmd("!$BROWSER" .. link)
+end
+
+require("which-key").add({ "<leader>o", group = "open" })
+vim.keymap.set("n", "<leader>op", open_phrends_document, {
+  desc = "Open Phrends document in localhost",
+  silent = false,
+})
